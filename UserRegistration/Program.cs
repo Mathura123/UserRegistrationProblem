8973f585c066b3a6524e5bdb90671a56c8bdf3ac
Mathura <mathuradas333@gmail.com>
2020-10-07 17:14:59 +0530

    [Mathura]Add. Ability to validate Last Name

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -8,18 +8,8 @@ namespace UserRegistration
         {
             Console.WriteLine("Welcome to User Registration Problem in Main Branch");
             Registration user = new Registration();
-        FirstName:
-            Console.WriteLine("Enter your First Name");
-            string firstName = Console.ReadLine();
-            if (user.validateFirstName(firstName) == true)
-            {
-                Console.WriteLine("Valid First Name");
-            }
-            else
-            {
-                Console.WriteLine("Invalid First Name\nTry again");
-                goto FirstName;
-            }
+            user.FirstName();
+            user.LastName();
         }
     }
 }
--- UserRegistration/Registration.cs
+++ UserRegistration/Registration.cs
@@ -7,10 +7,43 @@ namespace UserRegistration
 {
     class Registration
     {
-        string firstNameRegex = "^[A-Z][a-z]{3,}";
-        public bool validateFirstName(string firstName)
+        string firstNameRegex = "^[A-Z][a-z]{2,}";
+        string lastNameRegex = "^[A-Z][a-z]{2,}";
+        public void FirstName()
+        {
+            Console.WriteLine("Enter your First Name");
+            string firstName = Console.ReadLine();
+            if (ValidateFirstName(firstName) == true)
+            {
+                Console.WriteLine("Valid First Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid First Name\nTry again");
+                FirstName();
+            }
+        }
+        public void LastName()
+        {
+            Console.WriteLine("Enter your Last Name");
+            string lastName = Console.ReadLine();
+            if (ValidateLastName(lastName) == true)
+            {
+                Console.WriteLine("Valid Last Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Last Name\nTry again");
+                LastName();
+            }
+        }
+        private bool ValidateFirstName(string firstName)
         {
             return Regex.IsMatch(firstName, firstNameRegex);
+        }
+        private bool ValidateLastName(string lastName)
+        {
+            return Regex.IsMatch(lastName, lastNameRegex);
         }
     }
 }
