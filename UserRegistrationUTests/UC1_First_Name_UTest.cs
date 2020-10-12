fca75bca84f7f0fa74956ea5b0af308db29fb83b
Mathura <mathuradas333@gmail.com>
2020-10-12 09:35:54 +0530

    [Mathura] Test Case ram_Should_Retrun_Invalid_First_Name passed in TC1

--- UserRegistrationUTests/UC1_First_Name_UTest.cs
+++ UserRegistrationUTests/UC1_First_Name_UTest.cs
@@ -19,5 +19,19 @@ namespace UserRegistrationUTests
             //Assert
             Assert.AreEqual(expectedResult, result);
         }
+        [TestMethod]
+        public void ram_Should_Return_Inalid_First_Name()
+        {
+            //Arrange
+            string expectedResult = "Invalid First Name";
+            string inputFirstName = "ram";
+            UserRegistration.Registration regObj = new UserRegistration.Registration();
+
+            //Act
+            string result = regObj.FirstName(inputFirstName);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
     }
 }
